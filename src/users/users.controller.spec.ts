71c9c9bc9294029c2e4ca36a6574f8cebd93c1c5
twihike <43339061+twihike@users.noreply.github.com>
2021-07-04 22:22:51 +0900

    Format

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -41,9 +41,9 @@ describe('Users Controller', () => {
         }),
       ];
 
-      const findAll = jest.spyOn(service, 'findAll').mockReturnValue(
-        new Promise<User[]>((resolve) => resolve(result)),
-      );
+      const findAll = jest
+        .spyOn(service, 'findAll')
+        .mockReturnValue(new Promise<User[]>((resolve) => resolve(result)));
 
       expect(await controller.findAll()).toEqual(result);
       expect(findAll.mock.calls).toHaveLength(1);
@@ -61,9 +61,7 @@ describe('Users Controller', () => {
 
       const findOneByName = jest
         .spyOn(service, 'findOneByName')
-        .mockReturnValue(
-          new Promise<User>((resolve) => resolve(result)),
-        );
+        .mockReturnValue(new Promise<User>((resolve) => resolve(result)));
 
       expect(await controller.findOneByName(input)).toEqual(result);
       expect(findOneByName.mock.calls[0][0]).toEqual(input);
